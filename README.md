718ca1bb6b13efde22ea8e355d8ff530f3f941f4
Rusky <informatic5.0@gmail.com>
2023-09-24 23:57:40 +0100

    DONE!

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# TexasChainSawMassacre-PingTool
+# Texas Chainsaw Ping Tool
+
+## Introduction
+The Texas Chainsaw Ping Tool is designed to help you check the automatically selected game server for "The Texas Chain Saw Massacre" video game. This tool monitors DNS requests made by your machine and provides information about the game server, including ping times.
+
+## Prerequisites
+Before using this tool, make sure you have the following prerequisites installed:
+
+1. **Python 3:** If you don't have Python 3 installed, you can download it from the official website: [Python 3 Download](https://www.python.org/downloads/).
+
+2. **WinPcap:** WinPcap is a library required for packet capturing on Windows systems. You can download and install it from the following link: [WinPcap Download](https://www.winpcap.org/install/default.htm).
+
+## Usage Instructions
+1. Clone or download the Texas Chainsaw Ping Tool repository to your computer.
+
+2. Install the required Python packages by running the following command in the tool's directory:
+
+3. Run the tool using Python 3:
+
+4. The tool will start monitoring DNS requests made by your machine.
+
+5. When the game server for "The Texas Chain Saw Massacre" is detected, the tool will display information about the server, including the lobby ID and region.
+
+6. The tool will then perform three ping tests on the detected server and calculate the average ping time.
+
+7. The ping results will be color-coded as follows:
+- Green: Ping time between 0 and 50 ms
+- Yellow: Ping time between 51 and 80 ms
+- Red: Ping time higher than 80 ms
+
+8. After displaying the ping results, the tool will wait for 10 seconds before searching for the game server again.
+
+9. To stop the tool, press `Ctrl+C` in the terminal.
+
+That's it! You can use the Texas Chainsaw Ping Tool to monitor and evaluate the game server's ping performance and avoid laggy matches!
+
+If you encounter any issues or have questions, feel free to reach out for assistance.
+
+
---
+++ start.py
@@ -0,0 +1,122 @@
+import os
+import platform
+import subprocess
+import time
+import re
+from ping3 import ping, verbose_ping
+from scapy.all import sniff, DNS, DNSQR
+
+# Function to check and install a Python package if not installed
+def check_install_package(package_name):
+    try:
+        import importlib
+        importlib.import_module(package_name)
+    except ImportError:
+        print(f"{package_name} is not installed. Installing...")
+        subprocess.run(["pip", "install", package_name])
+
+# Check and install required packages
+check_install_package("ping3")
+check_install_package("scapy")
+
+# Define the list of strings to search for
+strings_to_search = [
+    "pfmsqosprod2-0.eastasia.cloudapp.azure.com",
+    "pfmsqosprod2-0.northeurope.cloudapp.azure.com",
+    "pfmsqosprod2-0.southeastasia.cloudapp.azure.com",
+    "pfmsqosprod2-0.brazilsouth.cloudapp.azure.com",
+    "pfmsqosprod2-0.eastus2.cloudapp.azure.com",
+    "pfmsqosprod2-0.eastus.cloudapp.azure.com",
+    "pfmsqosprod2-0.westus.cloudapp.azure.com",
+    "pfmsqosprod2-0.centralus.cloudapp.azure.com",
+    "pfmsqosprod2-0.westeurope.cloudapp.azure.com",
+    "pfmsqosprod2-0.australiaeast.cloudapp.azure.com",
+    "pfmsqosprod2-0.japaneast.cloudapp.azure.com",
+    "pfmsqosprod2-0.japanwest.cloudapp.azure.com"
+]
+
+# Additional pattern to search for
+additional_pattern = r"dns-([a-zA-Z0-9-]+)\.([a-zA-Z0-9-]+)\.cloudapp\.azure\.com"
+
+# Platform-specific console clear command
+clear_command = "cls" if platform.system() == "Windows" else "clear"
+
+def clear_console():
+    os.system(clear_command)
+
+# Clear the console and provide a notification
+clear_console()
+print("Scan started...")
+
+# ANSI color codes
+GREEN = "\033[32m"  # Green text
+YELLOW = "\033[33m"  # Yellow text
+RED = "\033[31m"     # Red text
+RESET_COLOR = "\033[0m"  # Reset text color
+
+def colorize_ping(ping_ms):
+    if 0 <= ping_ms <= 50:
+        return f"{GREEN}{ping_ms} ms{RESET_COLOR}"
+    elif 51 <= ping_ms <= 80:
+        return f"{YELLOW}{ping_ms} ms{RESET_COLOR}"
+    else:
+        return f"{RED}{ping_ms} ms{RESET_COLOR}"
+
+def dns_sniffer(pkt):
+    if DNS in pkt:
+        qname = pkt[DNSQR].qname.decode('utf-8')
+
+        # Check if there's a match in the initial search strings
+        for search_string in strings_to_search:
+            if search_string in qname:
+                print(f"Game server pinged: {search_string}")
+                break
+        
+        match = re.search(additional_pattern, qname)
+        if match:
+            os.system(clear_command)
+            game_id = match.group(1)
+            region = match.group(2)
+            print("--------------------------------------------------------")
+            print(f"\n Game Lobby with ID: {game_id} \n \n Region: {region}\n")
+            print("--------------------------------------------------------")
+            game_url = f"dns-{game_id}.{region}.cloudapp.azure.com"
+            ping_successful = True
+
+            # Perform three pings and calculate the average
+            ping_attempts = 3
+            total_ping_time = 0
+
+            for _ in range(ping_attempts):
+                ping_result = ping(game_url)
+                if ping_result is not None:
+                    total_ping_time += ping_result
+                    # Convert ping_result from seconds to milliseconds and round to 2 decimal places
+                    ping_result_ms = round(ping_result * 1000, 2)
+                    
+                    if ping_result_ms < 1:
+                        # If the ping time is less than 1 ms, it's likely a very quick response
+                        print("Ping:{GREEN}< 1 ms ms{RESET_COLOR}")
+                        time.sleep(3)
+                    else:
+                        colorized_ping = colorize_ping(ping_result_ms)
+                        print(f"Ping: {colorized_ping}")
+                        time.sleep(3)
+                else:
+                    print("Ping failed. Reverting to searching...")
+                    ping_successful = False
+
+            if ping_successful:
+                average_ping_ms = round((total_ping_time / ping_attempts) * 1000, 2)
+                colorized_average_ping = colorize_ping(average_ping_ms)
+                print(f"Average Ping: {colorized_average_ping}")
+                time.sleep(60)
+try:
+    # Start sniffing DNS packets
+    sniff(filter="udp port 53", prn=dns_sniffer, store=0)
+except Exception as e:
+    if "winpcap is not installed" in str(e):
+        os.system(clear_command)
+        print("Error: WinPcap is not installed. Please install WinPcap or a similar packet capture library to run this script.")
+    else:
+        raise e
